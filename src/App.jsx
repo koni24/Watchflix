f274fc8616798c9995b6809efc5aba960f6cda69
koni24 <konisha.24.kar@gmail.com>
2023-03-04 20:59:45 +0530

    genres created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -16,7 +16,8 @@ function App() {
   const { url } = useSelector((state) => state.home);
   console.log(url);
   useEffect(() => {
-    fetchApiConfig( );
+    fetchApiConfig();
+    genresCall();
   }, []);
   const fetchApiConfig = () => {
     fetchDataFromApi("/configuration").then((res) => {
@@ -29,6 +30,23 @@ function App() {
       dispatch(getApiConfiguration(url));
     });
   };
+
+  const genresCall = async()=>{
+    let promises=[];
+    let endPoints=["tv", "movie"];
+    let allGenres={};
+ 
+    endPoints.forEach((url)=>{
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+    const data=await Promise.all(promises);
+    console.log(data);
+    data.map(({genres}) => {
+      return genres.map((item)=>(allGenres[item.id]=item));
+    });
+
+    dispatch(getGenres(allGenres))
+  }
   return (
     <BrowserRouter>
       <Header/>
